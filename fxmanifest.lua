649c0d90621dce0865ed908ac7ef7f0675ee6fce
kibukj <kibuzhj@gmail.com>
2020-10-08 16:27:12 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version 'adamant'
 game 'common'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+server_only 'yes'
+
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'server.lua'
