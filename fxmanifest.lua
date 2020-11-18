f50c977130d86375b6cb46f2b4161a862dbd5050
kibukj <kibuzhj@gmail.com>
2020-11-18 13:44:19 -0500

    Remove rdr3_warning

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,5 @@
 fx_version 'adamant'
 game 'common'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 server_only 'yes'
 
